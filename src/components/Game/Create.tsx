4b41b13f0164a516e9a83e6a851f210d9f9fa0e1
Myk <mykcryptodev@gmail.com>
2024-09-02 10:57:22 -0400

    go to game after create

--- src/components/Game/Create.tsx
+++ src/components/Game/Create.tsx
@@ -1,18 +1,34 @@
-import { type FC } from "react";
+import { useRouter } from "next/router";
+import { type FC,useState } from "react";
 
 import { api } from "~/utils/api";
 
 const CreateGame: FC = () => {
+  const router = useRouter();
   const { mutateAsync: createGame } = api.game.create.useMutation();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
   return (
     <button
+      className="btn"
+      disabled={isLoading}
       onClick={async () => {
-        const game = await createGame({
-          name: "My Game",
-        });
-        console.log({ game });
+        setIsLoading(true);
+        try {
+          const game = await createGame({
+            name: "My Game",
+          });
+          void router.push(`/game/${game.id}`);
+        } catch (e) {
+          console.error(e);
+        } finally {
+          setIsLoading(false);
+        }
       }}
     >
+      {isLoading && (
+        <div className="loading loading-spinner" />
+      )}
       Create Game
     </button>
   );
