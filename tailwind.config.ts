5ef0ce0e0b566b6c7a7e0a644c32dd9a1cc93ac0
Myk <mykcryptodev@gmail.com>
2024-09-05 16:40:20 -0400

    add theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,5 +4,8 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.tsx"],
   theme: {},
+  daisyui: {
+    themes: ["lofi"],
+  },
   plugins: [daisyui],
 } satisfies Config;
