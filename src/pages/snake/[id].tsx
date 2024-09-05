a31a83c8b3f5e7f6a851c7a8d0848e03f84164f5
Myk <mykcryptodev@gmail.com>
2024-09-04 22:42:00 -0400

    show controls

--- src/pages/snake/[id].tsx
+++ src/pages/snake/[id].tsx
@@ -1,5 +1,5 @@
 import { type GetServerSideProps, type NextPage } from "next";
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import React, { type FC, useCallback, useEffect, useRef, useState } from "react";
 import { zeroAddress } from "viem";
 import { useAccount } from "wagmi";
 
@@ -191,12 +191,16 @@ const SnakeGame: NextPage<Props> = ({ id }) => {
       const getNextDirection = (key: string): Direction | null => {
         switch (key) {
           case "ArrowUp":
+          case "w":
             return { x: 0, y: -1 };
           case "ArrowDown":
+          case "s":
             return { x: 0, y: 1 };
           case "ArrowLeft":
+          case "a":
             return { x: -1, y: 0 };
           case "ArrowRight":
+          case "d":
             return { x: 1, y: 0 };
           default:
             return null;
@@ -246,6 +250,51 @@ const SnakeGame: NextPage<Props> = ({ id }) => {
   }, [handleKeyPress]);
 
   console.log({ userColors });
+
+  const StartGame: FC = () => {
+    return (
+      <div className="w-full h-full grid place-content-center">
+        <div className="font-mono text-black flex flex-col gap-2 mx-auto">
+          <div className="font-bold text-center">Controls</div>
+          <div className="flex flex-col sm:hidden w-[75px]">
+            <div className="flex w-full justify-between items-center text-sm">
+              <div>2</div><div>Up</div>
+            </div>
+            <div className="flex w-full justify-between items-center text-sm">
+              <div>8</div><div>Down</div>
+            </div>
+            <div className="flex w-full justify-between items-center text-sm">
+              <div>4</div><div>Left</div>
+            </div>
+            <div className="flex w-full justify-between items-center text-sm">
+              <div>6</div><div>Right</div>
+            </div>
+          </div>
+          <div className="sm:flex sm:flex-col hidden w-[150px]">
+            <div className="flex w-full justify-between items-center text-sm">
+              <div>&#8593; / W</div><div>Up</div>
+            </div>
+            <div className="flex w-full justify-between items-center text-sm">
+              <div>&#8595; / S</div><div>Down</div>
+            </div>
+            <div className="flex w-full justify-between items-center text-sm">
+              <div>&#8592; / A</div><div>Left</div>
+            </div>
+            <div className="flex w-full justify-between items-center text-sm">
+              <div>&#8594; / D</div><div>Right</div>
+            </div>
+          </div>
+        </div>
+        <div className="sm:hidden flex flex-col font-mono text-black text-center my-6">
+          <div className="font-bold">Tip</div>
+          <div>Tapping the numbers with your thumbs makes the game easier to play!</div>
+        </div>
+        <button onClick={startGame} className="btn btn-primary mt-2">
+          Start Game
+        </button>
+      </div>
+    )
+  }
 
   return (
     <div className="flex min-h-screen flex-col items-center">
@@ -309,11 +358,7 @@ const SnakeGame: NextPage<Props> = ({ id }) => {
               </div>
             </>
           ) : (
-            <div className="w-full h-full grid place-content-center">
-              <button onClick={startGame} className="btn btn-primary">
-                Start Game
-              </button>
-            </div>
+            <StartGame />
           )}
           {gameOver && (
             <div className="flex h-full items-center w-full absolute">
